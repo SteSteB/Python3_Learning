beb6e1687b188cc558164c4900ed07d778b161d6
Steven Briggs <steven@brig.gs>
2023-09-05 13:24:51 +0100

    first python work uploaded

---
+++ First.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+# Author: Steven Briggs
+# Version: 1.0
+# Description: This is or first program
+"""
+Docstring:
+"""
+
+#name = "Steven Briggs"
+name= input("Enter your name:")
+print("my name Is", name)
+
+import math
+print("Cosine of 0.5 is", math.cos(0.5))
+
+import random
+print("My lucky number is", random.randint(1,50))
