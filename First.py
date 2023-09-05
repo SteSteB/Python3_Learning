da648bcd4e7f3c67fc45154983a31656bd525aa4
Steven Briggs <steven@brig.gs>
2023-09-05 16:44:18 +0100

    end of first day of python

--- First.py
+++ First.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 # Author: Steven Briggs
 # Version: 1.0
-# Description: This is or first program
+# Description: This is our first program.
 """
 Docstring:
 """
@@ -15,3 +15,4 @@ print("Cosine of 0.5 is", math.cos(0.5))
 
 import random
 print("My lucky number is", random.randint(1,50))
+
